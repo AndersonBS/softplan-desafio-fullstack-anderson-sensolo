993f5c1889a1243f3114ae3ba15e37ab257c7a1f
Anderson B. Sensolo <anderson.sensolo@unoesc.edu.br>
2020-11-16 08:38:53 -0300

    Alterado e-mails dos usuários padrões

--- backend/src/main/resources/db/migration/versioned/2020/11/V0_0_1.09__INSERT_INTO_USUARIO.sql
+++ backend/src/main/resources/db/migration/versioned/2020/11/V0_0_1.09__INSERT_INTO_USUARIO.sql
@@ -1,4 +1,4 @@
 INSERT INTO USUARIO (NOME, LOGIN, SENHA, EMAIL, PERMISSAO, DATAINCLUSAO, VERSAO)
-VALUES ('Administrador', 'admin', '$2a$04$aHkXWAlSAAnWb0GpeSPbVew0UfReI2yBXFVaO9kLtFSC76aEgtYP6', 'admin@admin.com', 'ADMINISTRADOR', CURDATE(), 0),
-	('Triador', 'triad', '$2a$04$pv9quK6mjSFqd455uU5IYO5OT8xiuO8N1RFAKU7YuyN5/tMK/cFdO', 'triad@triad.com', 'TRIADOR', CURDATE(), 0),
-	('Finalizador', 'final', '$2a$04$00ViLCus29KwQmwVEpkQb.tG/Ncod9vcqsaRN5NkvmQ9kO7VBkeBG', 'final@final.com', 'FINALIZADOR', CURDATE(), 0);
+VALUES ('Administrador', 'admin', '$2a$04$aHkXWAlSAAnWb0GpeSPbVew0UfReI2yBXFVaO9kLtFSC76aEgtYP6', 'administrador@softplan.com', 'ADMINISTRADOR', CURDATE(), 0),
+	('Triador', 'triad', '$2a$04$pv9quK6mjSFqd455uU5IYO5OT8xiuO8N1RFAKU7YuyN5/tMK/cFdO', 'triador@softplan.com', 'TRIADOR', CURDATE(), 0),
+	('Finalizador', 'final', '$2a$04$00ViLCus29KwQmwVEpkQb.tG/Ncod9vcqsaRN5NkvmQ9kO7VBkeBG', 'finalizador@softplan.com', 'FINALIZADOR', CURDATE(), 0);
