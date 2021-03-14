a8ca6e8da0ac3d9cf400dd6c43b7ef909244fc93
Anderson B. Sensolo <anderson.sensolo@unoesc.edu.br>
2021-03-14 12:26:18 -0300

    Atualizado o ano do footer

--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -6,7 +6,7 @@ class FooterComponent extends Component {
         return (
             <footer className="footer fixed-bottom bg-dark text-center">
                 <span className="text-muted">
-                    © 2020 Copyright:
+                    © 2021 Copyright:
                     <a href="https://www.linkedin.com/in/andersonsensolo/" target="_blank"> Anderson B. Sensolo</a>
                 </span>
             </footer>
