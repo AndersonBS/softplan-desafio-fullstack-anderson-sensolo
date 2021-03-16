cb6e48b0f57830c66401ac67da9264da6879abd2
Anderson B. Sensolo <anderson.sensolo@unoesc.edu.br>
2021-03-16 00:08:24 -0300

    Adicionado animação 3D à página de login

--- frontend/src/components/SignInComponent.jsx
+++ frontend/src/components/SignInComponent.jsx
@@ -16,6 +16,46 @@ class SignInComponent extends Component {
         AuthenticationService.logout();
     }
 
+    componentDidMount() {
+        const cardEffect = document.querySelector(".card-effect");
+        const card = document.querySelector(".card");
+        const title = document.querySelector(".title");
+        const inputs = document.querySelector(".inputs");
+        const submit = document.querySelector(".submit");
+        const info = document.querySelector(".info");
+
+        cardEffect.addEventListener("mousemove", (e) => {
+            let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
+            let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
+            card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+        });
+
+        cardEffect.addEventListener("mouseenter", (e) => {
+            card.style.transition = "none";
+            title.style.transition = "none";
+            inputs.style.transition = "none";
+            submit.style.transition = "none";
+            info.style.transition = "none";
+            title.style.transform = "translateZ(50px)";
+            inputs.style.transform = "translateZ(45px)";
+            submit.style.transform = "translateZ(40px)";
+            info.style.transform = "translateZ(35px)";
+        });
+
+        cardEffect.addEventListener("mouseleave", (e) => {
+            card.style.transition = "all 0.5s ease";
+            title.style.transition = "all 0.5s ease";
+            inputs.style.transition = "all 0.5s ease";
+            submit.style.transition = "all 0.5s ease";
+            info.style.transition = "all 0.5s ease";
+            card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+            title.style.transform = "translateZ(0px)";
+            inputs.style.transform = "translateZ(0px)";
+            submit.style.transform = "translateZ(0px)";
+            info.style.transform = "translateZ(0px)";
+        });
+    }
+
     handleSignIn = async e => {
         e.preventDefault();
         const { username, password } = this.state;
@@ -34,22 +74,32 @@ class SignInComponent extends Component {
     render() {
         return (
             <div className="container">
-                {this.state.error && <div className="alert alert-danger" role="alert">{this.state.error}</div>}
-                <form className="form-signin" onSubmit={this.handleSignIn}>
-                    <h1 className="form-signin-heading text-muted">Softplan</h1>
-                    <input type="username" className="form-control" placeholder="Login"
-                            onChange={e => this.setState({ username: e.target.value })} />
-                    <input type="password" className="form-control" placeholder="Senha"
-                            onChange={e => this.setState({ password: e.target.value })} />
-                    <button className="btn btn-lg btn-primary btn-block" type="submit">
-                        Entrar
-                    </button>
-                    <br /><br /><br />
-                    <h6 className="form-signin-footer text-muted"><b>Login & Senha</b></h6>
-                    <h6 className="form-signin-footer text-muted">Administrador: admin</h6>
-                    <h6 className="form-signin-footer text-muted">Triador: triad</h6>
-                    <h6 className="form-signin-footer text-muted">Finalizador: final</h6>
-                </form> 
+                <div className="singin-container">
+                    <div class="card-effect">
+                        <div class="card">
+                            {this.state.error && <div className="alert alert-danger" role="alert">{this.state.error}</div>}
+                            <form className="form-signin" onSubmit={this.handleSignIn}>
+                                <h1 className="form-signin-heading text-muted title">Softplan</h1>
+                                <div className="inputs">
+                                    <input type="username" className="form-control" placeholder="Login"
+                                            onChange={e => this.setState({ username: e.target.value })} />
+                                    <input type="password" className="form-control" placeholder="Senha"
+                                            onChange={e => this.setState({ password: e.target.value })} />
+                                </div>
+                                <button className="btn btn-lg btn-primary btn-block submit" type="submit">
+                                    Entrar
+                                </button>
+                                <br /><br /><br />
+                                <div className="info">
+                                    <h6 className="form-signin-footer text-muted"><b>Login & Senha</b></h6>
+                                    <h6 className="form-signin-footer text-muted">Administrador: admin</h6>
+                                    <h6 className="form-signin-footer text-muted">Triador: triad</h6>
+                                    <h6 className="form-signin-footer text-muted">Finalizador: final</h6>
+                                </div>
+                            </form>
+                        </div> 
+                    </div>
+                </div> 
             </div>
         );
     }
